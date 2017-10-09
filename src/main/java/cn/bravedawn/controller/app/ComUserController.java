25f277a84bf60ca0749ca3fc645ffd5528499250
depers <dev_fengxiao@foxmail.com>
2017-10-09 21:39:04 +0800

    RestfulApi实现成功。

--- src/main/java/cn/bravedawn/controller/app/ComUserController.java
+++ src/main/java/cn/bravedawn/controller/app/ComUserController.java
@@ -1,10 +1,11 @@
 package cn.bravedawn.controller.app;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by 冯晓 on 2017/10/9.
  */
 @RestController
 public class ComUserController {
+
 }
